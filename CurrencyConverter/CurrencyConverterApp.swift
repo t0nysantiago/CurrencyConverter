03aef8ff70cc6af02d02afe7d6518d477809e74e
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-12-06 20:01:29 -0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Tony Santiago on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
